0934c3c43ba25a48bbb7d8eade59c8893ca95e24
Thijs Xhaflaire <txhaflaire@bol.com>
2018-12-12 18:53:34 +0100

    Update Self Service - Reset login keychain.sh

--- Self Service - Reset login keychain.sh
+++ Self Service - Reset login keychain.sh
@@ -3,8 +3,11 @@
 ## Written by Thijs Xhaflaire - 29-09-2017
 ## Purpose of this script is to reset the login keychain of the user and do a reboot afterwards so that a new keychain will be created on reboot.
 
+# Variables
+loggedInUser="$(python -c 'from SystemConfiguration import SCDynamicStoreCopyConsoleUser; import sys; username = (SCDynamicStoreCopyConsoleUser(None, None, None) or [None])[0]; username = [username,""][username in [u"loginwindow", None, u""]]; sys.stdout.write(username + "\n");')"
+
 # Removes loginkeychain from current logged in user
-rm -Rf /Users/$USER/Library/Keychains/*
+rm -Rf /Users/$loggedInUser/Library/Keychains/*
 
 # Take a nap
 sleep 5
